2eedf32ba85a9baaadaa7350c9a49679964d37f3
Taraneh Saleh <domina@Taranehs-MacBook-Pro.local>
2022-07-25 15:05:06 -0400

    committing last changes

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -6,7 +6,7 @@ import NavMbl from './NavMbl';
 
 const Header = () => {
   const [menuClass, setMenuClass] = useState("hide");
-  const [burger, setBurger] = useState("show");
+  const [burger, setBurger] = useState("mt-2 w-6 h-7");
   const [drop, setDrop] = useState("hide");
 
   const hideMenu = () => {
@@ -27,7 +27,10 @@ const Header = () => {
     <div className="Header flex justify-between md:mt-10 ">
         <nav className="nav hidden lg:flex lg:justify-between " >
           <ul className='flex cursor-pointer'>
-            <li className="text-2xl">Services & Pricing</li> 
+            <li className="text-2xl">
+              <Link to="ServiceSection" smooth="true"duration={1000}> Services & Pricing</Link>
+              
+             </li> 
             <li className="text-2xl ml-10">
               <Link to="About" smooth="true"duration={1000}>About</Link>
             </li> 
@@ -46,13 +49,43 @@ const Header = () => {
            </a>
         </div>
         <div className="nav-mobile lg:hidden h-10">
-        <button>
-             <svg className="mt-2 w-6 h-7" fill="none" stroke="currentColor" stroke-width="2">
+        <button className="" onClick={showMenu}>
+             <svg className={burger} 
+             
+             fill="none" stroke="currentColor" stroke-width="2">
               <path stroke-linecap="round" stroke-linejoin="round" d="M4 6h16M4 12h16M4 18h16" />
              </svg>
+             <svg
+          className={drop}
+            width="24"
+            height="30"
+            viewBox="0 0 24 38"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              d="M23.25 0.999999C23.25 0.861929 23.1381 0.75 23 0.75H12H1C0.861929 0.75 0.75 0.861928 0.75 1V5.90658C0.75 6.01888 0.824884 6.1174 0.933088 6.14746L11.9331 9.20302C11.9769 9.21518 12.0231 9.21518 12.0669 9.20302L23.0669 6.14746C23.1751 6.1174 23.25 6.01888 23.25 5.90658V0.999999Z"
+              fill="#54301A"
+              stroke="#54301A"
+              strokeWidth="1.5"
+            />
+            <path
+              d="M23.25 13C23.25 12.8619 23.1381 12.75 23 12.75H11.3333H0.999998C0.861929 12.75 0.75 12.8619 0.75 13V17.1358C0.75 17.2424 0.817549 17.3372 0.918253 17.372L11.9183 21.1781C11.9712 21.1964 12.0288 21.1964 12.0817 21.1781L23.0817 17.372C23.1825 17.3372 23.25 17.2424 23.25 17.1358V13Z"
+              fill="#54301A"
+              stroke="#54301A"
+              strokeWidth="1.5"
+            />
+            <path
+              d="M23.25 25C23.25 24.8619 23.1381 24.75 23 24.75H12H1C0.861929 24.75 0.75 24.8619 0.75 25V28.5C0.75 28.5787 0.787048 28.6528 0.85 28.7L11.85 36.95C11.9389 37.0167 12.0611 37.0167 12.15 36.95L23.15 28.7L23.6 29.3L23.15 28.7C23.213 28.6528 23.25 28.5787 23.25 28.5V25Z"
+              fill="#54301A"
+              stroke="#54301A"
+              strokeWidth="1.5"
+            />
+          </svg>
         </button>
         </div>
-        <NavMbl showMenu={menuClass} hideMenu={hideMenu}/>
+       
+        {/* <NavMbl showMenu={menuClass} hideMenu={hideMenu}/> */}
     </div>
   )
 }
--- src/components/NavMbl.jsx
+++ src/components/NavMbl.jsx
@@ -1,11 +1,38 @@
 import React from 'react'
+import { Link } from "react-scroll";
 
-function NavMbl() {
+function NavMbl(props) {
   return (
     <div className="MblNav showMenu lg:hidden ">
 
+        <ul className=''>
+        <li className="text-xl ">
+               <Link to="ServiceSection" smooth="true"duration={1000}> Services</Link>
+            </li>
+            <li className="text-xl">
+              <Link to="About" smooth="true"duration={1000}>About</Link>
+            </li> 
+        </ul>
         
-        
+      <button
+        onClick={props.hideMenu}
+        className="w-full text-lavender text-center"
+      >
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          className="h-6 w-6 ml-6"
+          fill="none"
+          viewBox="0 0 24 24"
+          stroke="currentColor"
+        >
+          <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            strokeWidth="2"
+            d="M13 5l7 7-7 7M5 5l7 7-7 7"
+          />
+        </svg>
+      </button>
     </div>
   )
 }
