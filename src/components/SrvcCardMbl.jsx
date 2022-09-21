05a36240c14e4471c71179caca7c4fd3fdc11e5d
Taraneh Saleh <domina@Taranehs-MacBook-Pro.local>
2022-09-21 09:23:18 -0400

    fix links on mobile

--- src/components/SrvcCardMbl.jsx
+++ src/components/SrvcCardMbl.jsx
@@ -9,39 +9,45 @@ function SrvcCardMbl() {
         description:"Botox injections block certain chemical signals from nerves, mostly signals that cause muscles to contract. The most common use of these injections is to temporarily relax the facial muscles that cause wrinkles in the forehead and around the eyes.",
         img:"../../public/img/botox.png",
         price:"$8 - $10/unit", 
-        duration:"30 minutes"
+        duration:"30 minutes",
+        book:"https://calendly.com/melavimelavi/neurotoxin-botox"
     },
     {
         name:"Derma Filler",
         description:"Dermal fillers are gel-like substances injected under the skin. Dermal fillers are meant to create a smoother or fuller appearance, or both.",
         img:"../../public/img/filler.png",
         price:"$500 - $600",
-        duration:"60 minutes"
+        duration:"60 minutes",
+        book:"https://calendly.com/melavimelavi/derma-filler-60-min"
     },
     {
         name:"Chemical Peels", 
         description:"This is a technique used to improve the texture of the skin. It is intended to remove the outermost layer of the skin.To accomplish this task, the chosen  peel solution induces a controlled injury to the skin. Resulting wound healing processes begin to regenerate new tissues. The dead skin eventually peels off.",
         img:"../../public/img/chemPeel.png",
         price:"$100 - $150", 
-        duration:"30 minutes"
+        duration:"30 minutes",
+        book:"https://calendly.com/melavimelavi/chemical-peels-30-min"
     },
     {
-        name:"Vitamine Injection", 
+        name:"Vitamin Injection", 
         description:"",
         img:"../../public/img/vitamineInjection.png",
         price:"", 
-        duration:"30 minutes"
+        duration:"30 minutes",
+        book:"https://calendly.com/melavimelavi/vitamin-injections-30"
     },
     {   
         name:"Fat Dissolving Injections", 
         description:"Fat-dissolving treatment, also known as lipolytic injections, is a simple and non-invasive cosmetic procedure that uses injections to target fat deposits in the body. These injections use Deoxycholic acids, a synthetic form of naturally-occurring acids in the body.",
         img:"../../public/img/fatDesolving.png",
         price:"$650/vial",
-        duration:"45 minutes"},
+        duration:"45 minutes",
+        book:"https://calendly.com/melavimelavi/fat-dissolving-solution-45-min"
+    },
   ]
   return (
     <div className="SrvcCardMbl md:hidden flex flex-wrap justify-around">
-       {services.map((srv)=> (<ServiceItem key= {srv.name} name={srv.name} description={srv.description} price={srv.price} duration={srv.duration} img={srv.img}/>))}
+       {services.map((srv)=> (<ServiceItem key= {srv.name} name={srv.name} description={srv.description} price={srv.price} duration={srv.duration} img={srv.img} book={srv.book}/>))}
 
        
     </div>
